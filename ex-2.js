a4538e6be82898dae2927a0df8b699b61db514f0
Pong <pongpong34756@gmail.com>
2024-05-09 16:18:46 +0700

    feat:Boolean

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,12 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = 4010;
+let isWeekday = "friday";
+let hasBoughtProductFromITCategory = "no";
+let hasAttendedDiscountEvent = "yes";
+let isPlatinum = "no";
 
-let hasPromotion;
+let hasPromotion = (((lastMonthPaidMoreThan4000 >= 4000) && (isWeekday == "monday" || isWeekday == "friday") && (hasBoughtProductFromITCategory == "no") && (hasAttendedDiscountEvent == "no")) ||(isPlatinum == "yes"));
 
 console.log(hasPromotion);
